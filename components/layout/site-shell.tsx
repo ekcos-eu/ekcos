b22ec66aaff195ea17950a3619930b90e1aa4be1
ton1czech <tonyasek007@gmail.com>
2026-08-18 17:39:33 +0200

    ✨ feat: homepage footer

--- components/layout/site-shell.tsx
+++ components/layout/site-shell.tsx
@@ -1,6 +1,5 @@
 import { SiteHeader } from '@/components/layout/site-header'
 import { SiteFooter } from '@/components/layout/site-footer'
-import { ConditionalFooter } from '@/components/layout/conditional-footer'
 import { NewsletterGate } from '@/components/newsletter/newsletter-gate'
 import { ShellFrame } from '@/components/layout/shell-frame'
 
@@ -9,9 +8,7 @@ export async function SiteShell({ children }: { children: React.ReactNode }) {
     <ShellFrame>
       <SiteHeader />
       <main className="flex min-h-0 flex-1 flex-col">{children}</main>
-      <ConditionalFooter>
-        <SiteFooter />
-      </ConditionalFooter>
+      <SiteFooter />
       <NewsletterGate />
     </ShellFrame>
   )
